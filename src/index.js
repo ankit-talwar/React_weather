7245af77cc4303b8b9d69c95c894e4bdaf6ea085
Ankit <ayushankit2009@gmail.com>
2020-04-06 15:52:37 +0530

    first comment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Loading from './Loading';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { lat: null, errorMessage: '' };
+
+
+    // }
+    state = { lat: null, errorMessage: '' };
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            postion => this.setState({ lat: postion.coords.latitude }),
+            err => this.setState({ errorMessage: 'user has denied the permission!' })
+        );
+
+    }
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div> Error:{this.state.errorMessage} </div>
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />;
+        }
+        else {
+            return <Loading message='accept location request to continue furthur' />;
+        }
+    }
+    render() {
+        return (
+            <div className='border red'>
+                {this.renderContent()}
+            </div>
+        )
+
+    }
+}
+
+ReactDom.render(<App />, document.querySelector('#root'));
